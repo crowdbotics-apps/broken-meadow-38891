47f0ef0b6e10b7216ad25e4cfc2333340a7931bc
Crowdbotics <team@crowdbotics.com>
2023-02-14 17:32:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-meadow-38891",
+  url: "https://broken-meadow-38891.botics.co",
+  api: "https://broken-meadow-38891.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
